ad1b4916e669ad4cb6c0483e88d6072ea3ef41a5
namlevu <namvule@hotmail.com>
2018-11-10 00:56:33 +0900

    fix route error

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import { Switch, BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+const Home=() => (
+  <div>
+    <h1>Welcome to the NamVL Website!</h1>
+  </div>
+)
+const About=()=>(
+  <div>
+    <h1>Welcome to the About page!</h1>
+  </div>
+)
+
 class App extends Component {
+
   render() {
     return (
       <Router>
         <Switch>
           <div>
-            <Route exact path="/" render={() => <h1>Home</h1>} />
-            <Route exact path="/about" render={() => <h1>About</h1>} />
-            <Route exact path="/posts" render={() => <h1>Posts</h1>} />
+            <Route exact path="/" component={Home}/>
+            <Route path="/about/" component={About} />
+            <Route path="/posts" render={() => <h1>Posts</h1>} />
           </div>
         </Switch>
       </Router>
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   module: {
     rules: [
@@ -10,6 +12,18 @@ module.exports = {
         }
       },
       {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [{
+            loader: 'css-loader',
+            options: {
+              minimize: true
+            }
+          }, 'sass-loader'],
+        }),
+      },
+      {
         test: /\.html$/,
         use: [
           {
@@ -19,7 +33,15 @@ module.exports = {
       }
     ]
   },
+  devServer: {
+    historyApiFallback: true,
+    publicPath: '/',
+    contentBase: './dist',
+  },
   plugins: [
+    new ExtractTextPlugin({
+      filename: 'style.css'
+    }),
     new HtmlWebPackPlugin({
       template: "./public/index.html",
       filename: "./index.html"

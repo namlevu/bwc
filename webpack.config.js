6e8e29f38b2805fb14cb08532aa47da6716e8a19
namlevu <namvule@hotmail.com>
2018-11-11 18:02:50 +0900

    temp commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,12 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  resolve:{
+  resolve: {
     extensions: ['.js', '.jsx'],
   },
 
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
@@ -19,21 +18,14 @@ module.exports = {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: [{
-            loader: 'css-loader',
-            options: {
-              minimize: true
-            }
-          }, 'sass-loader'],
-        }),
+          use: ['css-loader', 'sass-loader']
+        })
       },
       {
         test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
+        use: [{
+          loader: "html-loader"
+        }]
       }
     ]
   },
